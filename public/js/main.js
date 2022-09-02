aac84219e4d0b37d6e2dfbace39f582597bdf90a
Lucas Faustman <lucasfaustman@gmail.com>
2022-09-01 21:29:17 -0700

    Fixed front end issues with bills. Created a new ejs page called paidbills that holds paid bills for users. Also edited client side js to handle updated event listener for paid bills

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const deleteBtn = document.querySelectorAll('.del')
-const billItem = document.querySelectorAll('span.not')
+const billItem = document.querySelectorAll('.not')
 const billComplete = document.querySelectorAll('span.completed')
 
 Array.from(deleteBtn).forEach((el)=>{
@@ -17,7 +17,7 @@ Array.from(billComplete).forEach((el)=>{
 async function deleteBill(){
     const billId = this.parentNode.dataset.id
     try{
-        const response = await fetch('bills/deleteBill', {
+        const response = await fetch('deleteBill', {
             method: 'delete',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
@@ -33,9 +33,11 @@ async function deleteBill(){
 }
 
 async function markComplete(){
+    console.log('Button was pressed')
     const billId = this.parentNode.dataset.id
+    console.log(billId)
     try{
-        const response = await fetch('bills/markComplete', {
+        const response = await fetch('markPaid', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
@@ -70,10 +72,10 @@ async function markIncomplete(){
 
 const billLine = document.querySelectorAll('.billItem')
 
-function lineColor() {
-  const todaysDate = new Date()
-  dueDate > (todaysDate - 7) ? billLine.backgroundColor = yellow
-    : dueDate > (todaysDate - 3) ? billLine.backgroundColor = red  
-}
+// function lineColor() {
+//   const todaysDate = new Date()
+//   dueDate > (todaysDate - 7) ? billLine.backgroundColor = yellow
+//     : dueDate > (todaysDate - 3) ? billLine.backgroundColor = red  
+// }
 
-lineColor()
+// lineColor()
