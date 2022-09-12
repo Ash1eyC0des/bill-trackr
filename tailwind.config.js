77645cd2de45e714180ccf1c68603bbf1d87e6c9
Ashley Christman <dev@ashleychristman.com>
2022-09-12 17:03:17 -0400

    Redesign with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{ejs,js}", "./public/**/*.{ejs,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#FFF",
+        "mountain-meadow": {
+          DEFAULT: "#10B981",
+          100: "#E9FDE5",
+          200: "#B1F8B1",
+          300: "#7EF491",
+          400: "#4AEF81",
+          500: "#16EB81",
+          600: "#10B981",
+          700: "#0D9881",
+          800: "#0A7777",
+          900: "#074956",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
